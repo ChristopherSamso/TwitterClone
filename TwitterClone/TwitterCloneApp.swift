ecf04ac442ccbd49d14be6f910e1e6a216aa2d53
ChristopherSamso <chsa6996@colorado.edu>
2022-09-19 22:54:13 -0600

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Christopher Samso on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
